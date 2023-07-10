70760e3ef59b2e093062319b2661c1806ccc9c28
miskatkhan91 <96833498+miskatkhan91@users.noreply.github.com>
2023-07-10 20:00:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const btnEl = document.getElementById("btn");
+const jokeEl = document.getElementById("joke");
+
+const apiKey = "buFMXlbdGm4jRC0Ib53+yQ==jXoms8AybmtuoZ0I";
+const options = {
+    methood: "Get",
+    headers:{
+        "X-Api-Key": apiKey,
+},
+};
+
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit="
+
+
+async function getJoke(){
+    joke.innerText = "Updateing...";
+    btnEl.disabled = true;
+    btnEl.innerText = "Loading.."
+    const response = await fetch(apiURL, options);
+    const data = await response.json();
+
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke"
+   
+    jokeEl.innerText = data[0].joke;
+}
+btnEl.addEventListener("click", getJoke);
